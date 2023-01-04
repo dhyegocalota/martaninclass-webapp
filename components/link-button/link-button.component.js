4511d171295707b85486eefaaab88e4c9c66d3c1
Dhyego Calota <dhyegofernando@gmail.com>
2023-01-04 15:28:25 -0400

    so we updated links

--- components/link-button/link-button.component.js
+++ components/link-button/link-button.component.js
@@ -1,9 +1,19 @@
+import classnames from "classnames";
+
 import styles from "./link-button.module.css";
 
-export default function LinkButton({ className, children, to, ...restProps }) {
+export default function LinkButton({
+  className,
+  children,
+  to,
+  pulsing,
+  ...restProps
+}) {
   return (
     <a
-      className={`${styles.root} ${className}`}
+      className={classnames(styles.root, className, {
+        [styles.pulsing]: pulsing,
+      })}
       {...restProps}
       href={to}
       target="_blank"
