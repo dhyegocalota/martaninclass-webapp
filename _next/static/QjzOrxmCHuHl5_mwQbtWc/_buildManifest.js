2cd6de62e9890016f92cebae240006b65a22a091
dhyegocalota <dhyegofernando@gmail.com>
2024-01-19 00:22:35 +0000

    Deploying to gh-pages from @ dhyegocalota/martaninclass-webapp@ce0e79bdbd9b38b515158032622a5b0589d41d8d 🚀

---
+++ _next/static/QjzOrxmCHuHl5_mwQbtWc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e4b58c768187ac34.css","static/chunks/pages/index-baa87267d50d2355.js"],"/_error":["static/chunks/pages/_error-1995526792b513b2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
